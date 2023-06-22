d364d11f10e3594b40ce3d6fe3990bfff695e873
Joe Garcia <gijoe250@gmail.com>
2023-06-22 16:48:17 -0500

    adjusting efficiency of code

--- Arrays/BestTimeToBuyAndSellStock.java
+++ Arrays/BestTimeToBuyAndSellStock.java
@@ -12,44 +12,37 @@ public class BestTimeToBuyAndSellStock {
     /** Submitted Code
      * Code to copy paste into LeetCode
      */
-    public int maxProfit(int[] prices)  {
+    public static int maxProfit(int[] prices)  {
         int profit = 0;
 
         if (prices.length == 1){
             return 0;
         }
         int oldBuy = 0;
-        int oldSell = 0;
         int oldProfit = 0;
         int newBuy = prices[0];
         int newSell = 0;
         int newProfit = 0;
         for(int i = 1; i < prices.length; i++){
-            if (newSell < prices[i]){
-                newSell = prices[i];
-                continue;
-            }
             if(newBuy > prices[i] && newSell == 0){
                 newBuy = prices[i];
                 continue;
             }
-            if ((newBuy > prices[i] && newSell != 0 && oldBuy == 0)){
-                oldBuy = newBuy;
-                oldSell = newSell;
-                if(oldSell - oldBuy > 0){
-                    oldProfit = oldSell - oldBuy;
-                }
-                else {
-                    oldProfit = 0;
+            if ((newBuy > prices[i] && newSell != 0)){
+                if(oldBuy == 0 || newProfit > oldProfit){
+                    oldBuy = newBuy;
+                    oldProfit = newProfit;
                 }
                 newBuy = prices[i];
                 newSell = 0;
+                continue;
+            }
+            if (newSell < prices[i]){
+                newSell = prices[i];
+                newProfit = newSell - newBuy;
             }
         }
-
-        if(newSell == 0)
-            return oldProfit;
-        return profit;
+        return Math.max(oldProfit, newProfit);
     }
 
     /** Test Inputs
@@ -58,16 +51,22 @@ public class BestTimeToBuyAndSellStock {
     private static void testCodeInputs(){
         int[] prices = new int[]{7,1,5,3,6,4};
         int output = 5;
-        assertCode();
+        assertCode(prices, output);
 
         prices = new int[]{7,6,4,3,1};
         output = 0;
-        assertCode();
+        assertCode(prices, output);
+
+        prices = new int[]{2,1,2,1,0,1,2};
+        output = 2;
+        assertCode(prices, output);
     }
 
     /** Assertions
      * assertions for code
      */
-    private static void assertCode(){
+    private static void assertCode(int[] prices, int output){
+        int profit = maxProfit(prices);
+        assert profit == output : String.format("The profit %d does not match expected output %d", profit, output );
     }
 }
