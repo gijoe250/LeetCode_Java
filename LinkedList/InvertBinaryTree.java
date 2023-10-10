1b7de02621923d8dd49e60de4841391dfd730b26
Joe Garcia <gijoe250@gmail.com>
2023-10-09 23:52:45 -0500

    more setup for treenode

--- LinkedList/InvertBinaryTree.java
+++ LinkedList/InvertBinaryTree.java
@@ -11,26 +11,42 @@ public class InvertBinaryTree {
     }
     /** Submitted Code
      * Code to copy paste into LeetCode
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
      */
-    private static int removeDuplicates(int[] nums) {
+    public static TreeNode invertTree(TreeNode root) {
+
     }
+}
 
     /** Test Inputs
      * Add inputs to test
      */
     private static void testCodeInputs(){
-        assertCode();
-    }
-
-    /** Assertions
-     * assertions for code
-     */
 //    Input: root = [4,2,7,1,3,6,9]
 //    Output: [4,7,2,9,6,3,1]
 //    Input: root = [2,1,3]
 //    Output: [2,3,1]
 //    Input: root = []
 //    Output: []
-    private static void assertCode(){
+        assertCode();
+    }
+
+    /** Assertions
+     * assertions for code
+     */
+    private static void assertCode(TreeNode root){
+        TreeNode tempOutput = invertTree(root);
     }
 }
