17e0d600c698041f3e4131899ce41f98099d6385
Joe Garcia <gijoe250@gmail.com>
2023-10-10 23:49:28 -0500

    working on it

---
+++ Miscellaneous/ValidAnagram.java
@@ -0,0 +1,53 @@
+package Miscellaneous;
+
+import java.util.HashMap;
+
+public class ValidAnagram {
+    /** The LeetCode link
+     * https://leetcode.com/problems/valid-anagram/
+     */
+    public static void main(String[] args) {
+        System.out.println("Running the Assertions");
+        testCodeInputs();
+        System.out.println("Finished the Assertions Successfully");
+    }
+    /** Submitted Code
+     * Code to copy paste into LeetCode
+     */
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> anagram = new HashMap<Character, Integer>();
+        for (int i=0; i < s.length(); i++){
+            if (anagram.containsKey(s.charAt(i))){
+                int tempValue = anagram.get(s.charAt(i));
+                anagram.put(s.charAt(i), tempValue + 1);
+            }else {
+                anagram.put(s.charAt(i), 1);
+            }
+            if (anagram.containsKey(t.charAt(i))){
+                int tempValue = anagram.get(t.charAt(i));
+                anagram.put(t.charAt(i), tempValue - 1);
+            }else {
+                anagram.put(t.charAt(i), -1);
+            }
+        }
+
+        return true;
+    }
+
+    /** Test Inputs
+     * Add inputs to test
+     */
+    private static void testCodeInputs(){
+        assertCode();
+    }
+
+    /** Assertions
+     * assertions for code
+     */
+    private static void assertCode(){
+    }
+}
+
