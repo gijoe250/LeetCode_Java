6df8a7a99fc41f28f65f2c62c7ae439d800132d2
Joe Garcia <gijoe250@gmail.com>
2023-10-26 23:10:23 -0500

    setting up lowest common ancestor

---
+++ Miscellaneous/lowestCommonAncestor.java
@@ -0,0 +1,34 @@
+package Miscellaneous;
+
+public class lowestCommonAncestor {
+    /** The LeetCode link
+     * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+     */
+    public static void main(String[] args) {
+        System.out.println("Running the Assertions");
+        testCodeInputs();
+        System.out.println("Finished the Assertions Successfully");
+    }
+    /** Submitted Code
+     * Code to copy paste into LeetCode
+     */
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+    }
+
+    /** Test Inputs
+     * Add inputs to test
+     */
+    private static void testCodeInputs(){
+        //        Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+        //        Output: 6
+        //        Explanation: The LCA of nodes 2 and 8 is 6.
+        assertCode();
+    }
+
+    /** Assertions
+     * assertions for code
+     */
+    private static void assertCode(){
+    }
+}
