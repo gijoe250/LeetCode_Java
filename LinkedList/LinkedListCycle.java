d67a4752521f301d2f5642769ab780c5a0a54214
Joe Garcia <gijoe250@gmail.com>
2024-01-03 23:34:28 -0600

    setting up LinkedListCycle leetcode

---
+++ LinkedList/LinkedListCycle.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+public class LinkedListCycle {
+    /** The LeetCode link
+     * https://leetcode.com/problems/linked-list-cycle/
+     */
+    public static void main(String[] args) {
+        System.out.println("Running the Assertions");
+        testCodeInputs();
+        System.out.println("Finished the Assertions Successfully");
+    }
+    /** Submitted Code
+     * Code to copy paste into LeetCode
+     */
+    public boolean hasCycle(ListNode head) {
+    }
+
+    /** Test Inputs
+     * Add inputs to test
+     */
+    private static void testCodeInputs(){
+        assertCode();
+    }
+
+    /** Assertions
+     * assertions for code
+     */
+    private static void assertCode(){
+    }
+}
