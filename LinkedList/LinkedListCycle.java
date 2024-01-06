85ce33994fe7f391a801d2c179b29837210e0e57
Joe Garcia <gijoe250@gmail.com>
2024-01-06 13:40:40 -0600

    dsicarding 1st and 2nd idea and going with 3rd idea

--- LinkedList/LinkedListCycle.java
+++ LinkedList/LinkedListCycle.java
@@ -1,5 +1,7 @@
 package LinkedList;
 
+import java.util.LinkedList;
+
 public class LinkedListCycle {
     /** The LeetCode link
      * https://leetcode.com/problems/linked-list-cycle/
@@ -13,11 +15,26 @@ public class LinkedListCycle {
      * Code to copy paste into LeetCode
      */
     public boolean hasCycle(ListNode head) {
-        //if next is null return false
-        //maybe have another function to call itself if not false
-        //TODO: 1st idea use size to get the size of the linked list, if size is ever exceeded, there is a loop
-        //TODO: 2nd idea place a pointer on each location or a traveling point passed and store the value. if the value is found, check if the address matches the pointer
+        //base case
+        if (head == null){
+            return false;
+        }
+        //1st idea use size to get the size of the linked list, if size is ever exceeded, there is a loop
+        //check size of linked-list; misunderstood what size does: failed
         //TODO: 3rd idea have a key value pair and store the value and address. if both value is hit, compare the addresses
+        //create new pointer
+        ListNode tracker = head;
+        //iterate through list exiting if a null is hit
+        while(tracker.next != null){
+            //check if current value exists
+            //if so check if addresses equal each other
+            //on yes, return true
+            //TODO:on no, store temp value in key value pair
+            //iterate to next node
+            tracker = tracker.next;
+        }
+        //since it has exited the loop, it is false
+        return false;
     }
 
     /** Test Inputs
