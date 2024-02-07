b041e069f99645d8061e5320bd7aa73b75de9aec
Joe Garcia <gijoe250@gmail.com>
2024-02-06 23:58:12 -0600

    explaining different functions

--- Miscellaneous/MyQueue.java
+++ Miscellaneous/MyQueue.java
@@ -2,22 +2,26 @@ package Miscellaneous;
 
 public class MyQueue {
 
+    //constructor
     public MyQueue() {
 
     }
-
+    //needs tp put something on a stack
     public void push(int x) {
 
     }
-
+    //needs to take off the stack
     public int pop() {
+        int poppedValue = 0;
 
+        return poppedValue;
     }
 
+    //look at current value
     public int peek() {
 
     }
-
+    //check if queue is empty or not
     public boolean empty() {
 
     }
